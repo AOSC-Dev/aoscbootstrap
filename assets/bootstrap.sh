35052df01d01f0b8bf39926a173fa1230ed9f3b0
liushuyu <liushuyu011@gmail.com>
2021-05-14 03:21:04 -0600

    assets: fix an issue with the cleanup scripts ...
    ... and add colors to the installation scripts

--- assets/bootstrap.sh
+++ assets/bootstrap.sh
@@ -8,20 +8,20 @@ PACKAGES=(
 length=${#PACKAGES[@]}
 for p in "${PACKAGES[@]}"; do
 count=$((count+1))
-echo "[$count/$length] Installing ${p}..."
+echo -e "\e[1m[$count/$length] Installing ${p}...\e[0m"
 dpkg --force-depends --unpack "/var/cache/apt/archives/${p}"
 done
 count_c=1;length_c=$(dpkg -l | grep -c 'iU')
 function dpkg_progress () {
     while read action step package; do
 if [ "$action" = 'processing:' ] && [ "$step" = 'configure:' ]; then
-echo "[$count_c/$length_c] Configuring $package...";count_c=$(( count_c + 1 ))
+echo -e "\e[1m\e[96m[$count_c/$length_c] Configuring $package...\e[0m";count_c=$(( count_c + 1 ))
 fi
     done
 }
 { dpkg --status-fd=7 --configure --pending --force-configure-any --force-depends 7>&1 >&8 | dpkg_progress; } 8>&1 \
 || { echo 'Configuring missed packages ...'; dpkg --configure -a; }
-echo 'Copying skeleton files ...'
+echo -e '\e[1m\e[94mCopying skeleton files ...\e[0m'
 cp -rvT /etc/skel /root
-echo 'Enabling systemd vendor presets ...'
+echo -e '\e[1m\e[94mEnabling systemd vendor presets ...\e[0m'
 systemctl preset-all
--- assets/cleanup.sh
+++ assets/cleanup.sh
@@ -21,20 +21,20 @@ WHITELIST="^/dev
     ALL_FILES="$(mktemp)"
     RM_FILES="$(mktemp)"
     PATTERN_FILES="$(mktemp)"
-    echo 'Listing dpkg packages ...'
-    mapfile -t PACKAGES <(dpkg-query --show --showformat="\${Package}\n")
-    echo 'Collecting files from dpkg ...'
+    echo -e '\e[1mListing dpkg packages ...\e[0m'
+    mapfile -t PACKAGES < <(dpkg-query --show --showformat="\${Package}\n")
+    echo -e '\e[1mCollecting files from dpkg ...\e[0m'
     find / -mindepth 2 >> "$ALL_FILES"
     dpkg-query --listfiles "${PACKAGES[@]}" > "$DPKG_FILES"
-    echo "$WHITELIST" > "$PATTERN_FILES"
+    echo -e "$WHITELIST" > "$PATTERN_FILES"
     grep -vEf "$PATTERN_FILES" < "$ALL_FILES" > "${ALL_FILES}.new"
     mv "${ALL_FILES}.new" "$ALL_FILES"
     grep -vxFf "$DPKG_FILES" < "$ALL_FILES" > "$RM_FILES"
-    echo 'Removing files ...'
+    echo -e '\e[1mRemoving files ...\e[0m'
     xargs -L 1000 -a "$RM_FILES" rm -rfv
     rm -fv "$ALL_FILES" "$DPKG_FILES" "$RM_FILES"
 }
 
 set -eo pipefail
-echo 'Cleaning up the installation ...'
+echo -e '\e[1mCleaning up the installation ...\e[0m'
 remove_files
