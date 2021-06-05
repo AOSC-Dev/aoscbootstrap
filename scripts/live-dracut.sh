26a0929a4d3c4c1a1b94648044a4f9301f635a63
Mingcong Bai <jeffbai@aosc.xyz>
2021-06-04 22:46:21 -0700

    scripts: LiveKit preparation

---
+++ scripts/live-dracut.sh
@@ -0,0 +1,13 @@
+echo "Generating a LiveKit initramfs ..."
+dracut --add "dmsquash-live livenet" "/live-initramfs.img"
+
+echo "Moving kernel image out ..."
+mv -v /boot/vmlinu* /kernel
+
+echo "Enabling auto-login ..."
+cat > /etc/systemd/system/getty@tty1.service.d/override.conf << EOF
+[Service]
+Type=simple
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear %I 38400 linux
+EOF
