86189e9f405e94bcee7a8656c91c2cf665539f7a
Mingcong Bai <jeffbai@aosc.xyz>
2021-06-06 01:46:34 -0700

    scripts/livekit: generate a motd

--- scripts/livekit.sh
+++ scripts/livekit.sh
@@ -14,3 +14,29 @@ Type=simple
 ExecStart=
 ExecStart=-/sbin/agetty --autologin root --noclear %I 38400 linux
 EOF
+
+echo "Cutting out unwanted files ..."
+rm -r /usr/{include,src,share/{doc,gtk-doc}}
+rm `find /usr/lib -name '*.a'`
+
+echo "Generating /etc/motd ..."
+cat > /etc/motd << EOF
+Welcome to AOSC OS LiveKit!
+
+Here you may find basic tools to install AOSC OS, or rescue other operating
+systems installed on your computer. Here below is a basic guide to preinstalled
+applications (in the form of commands) on LiveKit:
+
+- deploykit: AOSC OS installer (EXPERIMENTAL).
+- cfdisk: Disk partition manager.
+- nmtui: Network (Ethernet, Wi-Fi, etc.) connection manager.
+- w3m: Web browser.
+
+If you have encountered any issue, please get in touch with us:
+
+- IRC: #aosc on Libera.Chat
+- GitHub: https://github.com/AOSC-Dev/aosc-os-abbs/issues/new/
+
+Enjoy your stay!
+
+EOF
