ee808602ec2b5e99218851d865c0078a2c8cb2c3
Mingcong Bai <jeffbai@aosc.xyz>
2022-01-29 03:36:54 +0000

    contrib/generate-releases: properly introduce Retro support
    - Use RETRO=1 to enter Retro mode.

--- contrib/generate-releases.sh
+++ contrib/generate-releases.sh
@@ -24,34 +24,38 @@ if [[ $# -eq 0 || "$1" == "--help" || "$1" == "-h" ]]; then
 fi
 
 export TZ='UTC'
-export RETRO=false
 
 mkdir -pv os-${ARCH:-$(dpkg --print-architecture)}
 
 for i in $@; do
-    if ! $RETRO; then
+    if [[ "$RETRO" != "1" ]]; then
         mkdir -pv os-${ARCH:-$(dpkg --print-architecture)}/$i
         aoscbootstrap \
+	    stable $i ${REPO:-https://repo.aosc.io/debs} \
             --config /usr/share/aoscbootstrap/config/aosc-mainline.toml \
             -x \
             --arch ${ARCH:-$(dpkg --print-architecture)} \
             -s \
                 /usr/share/aoscbootstrap/scripts/reset-repo.sh \
+	    -s \
                 /usr/share/aoscbootstrap/scripts/enable-nvidia-drivers.sh \
+	    -s \
                 /usr/share/aoscbootstrap/scripts/enable-dkms.sh \
             --include-files /usr/share/aoscbootstrap/recipes/$i.lst \
-            --export-tar os-${ARCH:-$(dpkg --print-architecture)}/$i/aosc-os_${i}_$(date +%Y%m%d)_${ARCH:-$(dpkg --print-architecture)}.tar.xz \
-            stable $i ${REPO:-https://repo.aosc.io/debs}
+	    --export-tar os-${ARCH:-$(dpkg --print-architecture)}/$i/aosc-os_${i}_$(date +%Y%m%d)_${ARCH:-$(dpkg --print-architecture)}.tar.xz \
+	    --export-squashfs os-${ARCH:-$(dpkg --print-architecture)}/$i/aosc-os_${i}_$(date +%Y%m%d)_${ARCH:-$(dpkg --print-architecture)}.squashfs
     else
+	echo "Generating for Retro ..."
         mkdir -pv os-${ARCH:-$(dpkg --print-architecture)}/$i
         aoscbootstrap \
+            stable $i ${REPO:-https://repo.aosc.io/debs-retro} \
             --config /usr/share/aoscbootstrap/config/aosc-retro.toml \
             -x \
             --arch ${ARCH:-$(dpkg --print-architecture)} \
             -s /usr/share/aoscbootstrap/scripts/reset-repo.sh \
             --include-files /usr/share/aoscbootstrap/recipes/$i.lst \
-            --export-tar os-${ARCH:-$(dpkg --print-architecture)}/$i/aosc-os-retro_${i}_$(date +%Y%m%d)_${ARCH:-$(dpkg --print-architecture)}.tar.xz \
-            stable $i ${REPO:-https://repo.aosc.io/debs-retro}
+	    --export-tar os-${ARCH:-$(dpkg --print-architecture)}/$i/aosc-os-retro_${i}_$(date +%Y%m%d)_${ARCH:-$(dpkg --print-architecture)}.tar.xz
+	    --export-squashfs os-${ARCH:-$(dpkg --print-architecture)}/$i/aosc-os_${i}_$(date +%Y%m%d)_${ARCH:-$(dpkg --print-architecture)}.squashfs
     fi
     rm -r $i
 
