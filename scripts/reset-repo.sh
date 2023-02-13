4fcb297f336a54029456acc941f1d42eab0e6f8f
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-13 11:00:32 -0800

    scripts/reset-repo.sh: fallback to heredoc generation if apt-gen-list is not available
    Unfortunately, Rust is not yet a universal human right in 2023.

--- scripts/reset-repo.sh
+++ scripts/reset-repo.sh
@@ -1,2 +1,8 @@
 echo "Resetting APT sources.list ..."
-apt-gen-list reset-mirror
+if command -v apt-gen-list > /dev/null; then
+	apt-gen-list reset-mirror
+else
+	cat > /etc/apt/sources.list << EOF
+deb https://repo.aosc.io/debs stable main
+EOF
+fi
