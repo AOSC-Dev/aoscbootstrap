0b3c35d4042a008ec0f45d1ed49cec0217076c15
liushuyu <liushuyu011@gmail.com>
2023-03-19 17:23:47 -0600

    assets/cleanup.sh: port ciel's new factory-reset script

--- assets/cleanup.sh
+++ assets/cleanup.sh
@@ -17,21 +17,23 @@ WHITELIST="^/dev
 ^/proc
 ^/sys
 /\.updated$"
+    local DPKG_FILES ALL_FILES RM_FILES PATTERN_FILES
+    local FIND_PID
     DPKG_FILES="$(mktemp)"
     ALL_FILES="$(mktemp)"
     RM_FILES="$(mktemp)"
     PATTERN_FILES="$(mktemp)"
-    echo -e '\e[1mListing dpkg packages ...\e[0m'
-    mapfile -t PACKAGES < <(dpkg-query --show --showformat="\${Package}\n")
     echo -e '\e[1mCollecting files from dpkg ...\e[0m'
-    find / -mindepth 2 >> "$ALL_FILES"
-    dpkg-query --listfiles "${PACKAGES[@]}" > "$DPKG_FILES"
-    echo -e "$WHITELIST" > "$PATTERN_FILES"
+    find / -mindepth 2 >> "$ALL_FILES" &
+    FIND_PID="$!"
+    cat /var/lib/dpkg/info/*.list > "$DPKG_FILES"
+    wait "$FIND_PID"
+    echo "$WHITELIST" > "$PATTERN_FILES"
     grep -vEf "$PATTERN_FILES" < "$ALL_FILES" > "${ALL_FILES}.new"
     mv "${ALL_FILES}.new" "$ALL_FILES"
     grep -vxFf "$DPKG_FILES" < "$ALL_FILES" > "$RM_FILES"
     echo -e '\e[1mRemoving files ...\e[0m'
-    xargs -L 1000 -a "$RM_FILES" rm -rfv
+    xargs -a "$RM_FILES" rm -rfv
     rm -fv "$ALL_FILES" "$DPKG_FILES" "$RM_FILES"
     # Remove some extra files that absolutely should not be in the release files.
     echo -e '\e[1mRemoving sensitive files ...\e[0m'
