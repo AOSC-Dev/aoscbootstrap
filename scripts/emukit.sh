9f0f8e106c3056de98276c34309d20b8e238c5b7
Mingcong Bai <jeffbai@aosc.xyz>
2023-10-27 11:18:46 -0700

    scripts/emukit.sh: handle ld-linux.so.2
    This was an absolute symlink, which will not work for a runtime sysroot, as
    the base path changes.

--- scripts/emukit.sh
+++ scripts/emukit.sh
@@ -9,3 +9,9 @@ for i in lib*GL*; do
 		ln -sv glvnd/$i /usr/lib/$i
 	fi
 done
+
+echo "Fixing up /opt/32/lib/ld-linux.so.2 symlink ..."
+ln -sv ../opt /usr/opt
+rm -v /lib/ld-linux.so.2
+ln -sv ../opt/32/lib/ld-linux.so.2 \
+    /lib/ld-linux.so.2
