39b0978b3d6c235588d0851dcf25e9f61957d740
Mingcong Bai <jeffbai@aosc.io>
2024-09-13 18:53:15 +0800

    fix(scripts/desktop.sh): generate catalog before package removal
    So that even when plasma-workspace-wallpapers were not installed, oma could
    still complete the removal procedure (showing a "package A is not installed"
    status) successfully.

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -1,2 +1,6 @@
 echo "Removing Plasma wallpapers ..."
+# So that even when plasma-workspace-wallpapers were not installed, oma could
+# still complete the removal procedure (showing a "package A is not installed"
+# status) successfully.
+oma refresh
 oma purge plasma-workspace-wallpapers --yes
