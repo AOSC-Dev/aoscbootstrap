aa6aa60e73ae45ef2f7fa8c560de4312bf0ad847
Mingcong Bai <jeffbai@aosc.io>
2024-09-13 20:18:19 +0800

    fix(desktop.sh): use dpkg to purge plasma-workspace-wallpapers

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -1,6 +1,2 @@
 echo "Removing Plasma wallpapers ..."
-# So that even when plasma-workspace-wallpapers were not installed, oma could
-# still complete the removal procedure (showing a "package A is not installed"
-# status) successfully.
-oma refresh
-oma purge plasma-workspace-wallpapers --yes
+dpkg --purge plasma-workspace-wallpapers
