c595b658f991939f3023ca964d31c30f9c173bfc
Errant404 <erigidissimus@gmail.com>
2024-10-25 18:31:29 +0800

    scripts/rawimage.sh: relax systemctl errors

--- scripts/rawimage.sh
+++ scripts/rawimage.sh
@@ -5,6 +5,7 @@ usermod -a -G audio,cdrom,video,wheel aosc
 echo "Setting default password ..."
 echo 'aosc:anthon' | chpasswd -c SHA512 -R /
 
+# FIXME: rootfs-grow.service has been deprecated.
 echo "Enabling RootFS adjustment service ..."
 touch /.rootfs-repartition
-systemctl enable rootfs-grow.service
+systemctl enable rootfs-grow.service || true
