9bb70228296fe2d31d0e557d786383d41a85ce1e
Kexy Biscuit a.k.a. るる <kexybiscuit@aosc.io>
2026-03-29 16:14:45 +0800

    feat: adjust WSL workarounds, mask systemd-firstboot.service only
    Co-authored-by: Xinhui Yang <cyan@cyano.uk>

--- scripts/wsl-workarounds.sh
+++ scripts/wsl-workarounds.sh
@@ -1,5 +1,2 @@
-echo "Creating an empty /etc/machine-id file for WSL ..."
-touch /etc/machine-id
-
-echo "Masking tmp.mount and user-runtime-dir@.service for WSLg ..."
-systemctl mask tmp.mount user-runtime-dir@.service
+echo "Masking systemd-firstboot.service for WSL ..."
+systemctl mask systemd-firstboot.service
